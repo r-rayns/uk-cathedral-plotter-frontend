5ae1a0c78c6f6b2a3e8c63bab1e2d9a14c184142
Robert <r.raynsford@gmail.com>
2025-04-21 09:47:55 +0100

    feat: make map legend collapsible
    - Make the map legend collapsible to give users the choice of hiding the legend to improve visibility of the map
    - Animate the transition between collapsed and expanded states

--- src/components/mapLegend.tsx
+++ src/components/mapLegend.tsx
@@ -1,18 +1,34 @@
-import type { LeafletPositionClass } from "@/models/leaflet";
+import { useState } from 'react';
 import type { ReactNode } from 'react';
+import type { LeafletPositionClass } from "@/models/leaflet";
 
 export function MapLegend({entries, position}: MapLegendProps) {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
+  function toggleCollapsed() {
+    setIsCollapsed(!isCollapsed);
+  }
 
   return (
-    <section className={["leaflet-control backdrop-blur-sm bg-white/50 m-2 md:m-4 p-2 md:p-4", ...position].join(' ')}>
-      <h4 className="my-0">Legend</h4>
-      <div className="flex flex-col gap-2">
-        {entries.map(({label, icon}, index) => (
-          <div key={index} className="flex items-center gap-2">
-            {icon}
-            {label}
-          </div>
-        ))}
+    <section className={[...position].join(' ')}>
+      <div className={["leaflet-control leaflet-bar backdrop-blur-xs bg-blue-100/40 text-slate-800 py-1"].join(' ')}>
+        <div onClick={toggleCollapsed}
+             className="flex flex-row px-2 justify-between hover:backdrop-blur-xl gap-2 hover:cursor-pointer hover:text-black">
+          <h4 className="my-0 text-center ">Legend</h4>
+          <span className={`text-base transition-[rotate] duration-300 ${isCollapsed ? 'rotate-180' : 'rotate-0'}`}>▲</span>
+        </div>
+        <div className={`flex flex-col gap-2 transition-[max-height] duration-500 ease-in-out px-2 scroll-gutter-stable
+          ${isCollapsed
+          ? 'max-h-0 overflow-y-hidden'
+          : 'max-h-[300px] overflow-y-auto'}`
+        }>
+          {entries.map(({label, icon}, index) => (
+            <div key={index} className="flex items-center gap-2">
+              {icon}
+              {label}
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   )
@@ -27,4 +43,3 @@ export interface MapLegendProps {
   entries: Array<MapLegendEntry>
   position: [( LeafletPositionClass.TOP | LeafletPositionClass.BOTTOM ), ( LeafletPositionClass.LEFT | LeafletPositionClass.RIGHT )]
 }
-
